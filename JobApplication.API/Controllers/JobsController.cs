ced6442907e858b9d675c9f563d5074c353a68ad
Alaa Hakeem <alaahakeem58@gmail.com>
2026-09-20 17:51:39 +0300

    Initial commit

---
+++ JobApplication.API/Controllers/JobsController.cs
@@ -0,0 +1,29 @@
+﻿using JobApplication.Application.DTOs;
+using JobApplication.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly IJobService _JobService;
+
+        public JobsController(IJobService jobService)
+        {
+            _JobService = jobService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateJobDto createJobDto)
+        {
+            var id = await _JobService.CreateAsync(createJobDto);
+            return Ok(new
+            {
+                id = id 
+            }); 
+        }
+    }
+}
---
+++ JobApplication.Application/Services/JobService.cs
@@ -0,0 +1,33 @@
+﻿using JobApplication.Application.DTOs;
+using JobApplication.Application.Interfaces;
+using JobApplication.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobApplication.Application.Services
+{
+    public class JobService : IJobService
+    {
+        private readonly IRepository<Job> _jobRepository;
+
+        public JobService(IRepository<Job> jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
+
+        public async Task<int> CreateAsync(CreateJobDto createJobDto)
+        {   
+            var job = new Job()
+            {
+                Title = createJobDto.Title,
+                Description = createJobDto.Description,
+                IsActive = true
+            };
+            await _jobRepository.AddAsync(job);
+            await _jobRepository.SaveChangesAsync();
+
+            return job.Id; 
+        }
+    }
+}
